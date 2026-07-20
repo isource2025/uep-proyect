c7a948611e487cd9eee00883f5000003c48ed692
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2026-07-20 10:50:44 -0300

    add comment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "./generated/prisma";
 import { PrismaMssql } from "@prisma/adapter-mssql";
 
+// Cache PrismaClient in development mode (touch for schema reloads)
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
 
 function parseSqlServerUrl(url: string) {
